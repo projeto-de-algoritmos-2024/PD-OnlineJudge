fab5eee3e86861d27c49ca21d1dad415d5fb452b
JoaoBarreto03 <211039519@aluno.unb.br>
2024-09-08 10:03:03 -0300

    Removendo counter e tentando nova implementação WA

--- Questão 2/Leetcode115.py
+++ Questão 2/Leetcode115.py
@@ -1,21 +1,22 @@
-from collections import Counter
-
 class Solution(object):
     def numDistinct(self, s, t):
-        # Contagem das letras em s e t
-        count_s = Counter(s)
-        count_t = Counter(t)
+        count = 0  # Contador para o número de vezes que `t` pode ser formado
+        s_list = list(s)  # Converte `s` em uma lista para manipulação direta
         
-        # Inicializa o número máximo de vezes que t pode ser formado
-        max_count = float('inf')
-        
-        # Verifica cada letra em t
-        for char in count_t:
-            if char in count_s:
-                # Calcula quantas vezes a letra pode ser usada para formar t
-                max_count = min(max_count, count_s[char] // count_t[char])
+        while True:
+            j = 0  # Índice para percorrer `t`
+            i = 0  # Índice para percorrer `s_list`
+            
+            while i < len(s_list) and j < len(t):
+                if s_list[i] == t[j]:
+                    s_list[i] = None  # Marca o caractere como usado
+                    j += 1  # Avança no `t` quando há correspondência
+                i += 1
+            
+            # Se conseguimos formar `t`, incrementamos o contador
+            if j == len(t):
+                count += 1
             else:
-                # Se a letra de t não estiver em s, não é possível formar t
-                return 0
+                break  # Se `t` não puder ser formado, saímos do loop
         
-        return max_count
+        return count
